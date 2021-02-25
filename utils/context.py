5dad2d36415b42f720eb84ea484fb9d05d9e08bd
kal-byte <kal@kal-byte.co.uk>
2021-02-25 17:49:54 +0000

    Test

--- utils/context.py
+++ utils/context.py
@@ -4,7 +4,10 @@ from discord.ext import commands
 class Context(commands.Context):
 
     def __truediv__(self, content: str = None):
-        return self.bot.loop.run_until_complete(self.send(content))
+        return self.bot.loop.create_task(self.send(content))
+
+    def __lshift__(self, content: str = None):
+        return self.__truediv__(content)
 
     def __repr__(self):
         return "<NoOneCares at 0xShutUp"
