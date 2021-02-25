377fd967b28f8541d92d88a4ef374bde8e28b823
kal-byte <kal@kal-byte.co.uk>
2021-02-25 17:27:36 +0000

    Test

--- utils/context.py
+++ utils/context.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 
 
-class Context(commands.Context)
+class Context(commands.Context):
 
     def __div__(self, content: str = None):
         return self.bot.loop.create_task(self.send(content))
