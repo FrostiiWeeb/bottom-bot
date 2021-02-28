c2a41c7994527b5bb08161d27b9caf2ecd9a09a0
kal-byte <kal@kal-byte.co.uk>
2021-02-28 21:26:34 +0000

    Testing time converter

--- cogs/owner.py
+++ cogs/owner.py
@@ -2,15 +2,95 @@
 
 from contextlib import redirect_stdout
 from discord.ext import commands
+from datetime import datetime as dt
+from typing import NamedTuple
 import subprocess
 import functools
+import datetime
 import textwrap
 import discord
 import utils
 import io
+import re
+
+
+class TimeReason(NamedTuple):
+    reason: str
+    time: dt
+
+    def __repr__(self) -> str:
+        return f"<TimeReason {reason} time={time!r}>"
+
+    def __str__(self) -> str:
+        now = dt.utcnow()
+        in_time = self.time - now
+        fmt = []
+
+        if y := in_time.year:
+            plural = "years" if y > 1 else "year"
+            fmt.append(f"{y} {plural}")
+
+        if m := in_time.month:
+            plural = "month" if y > 1 else "month"
+            fmt.append(f"{m} {plural}")
+
+        if d := in_time.day:
+            plural = "days" if y > 1 else "day"
+            fmt.append(f"{d} {plural}")
+
+        if h := in_time.hour:
+            plural = "hours" if y > 1 else "hour"
+            fmt.append(f"{h} {plural}")
+
+        if m := in_time.minute:
+            plural = "minutes" if y > 1 else "minute"
+            fmt.append(f"{m} {plural}")
+
+        return ", ".join(fmt)
+
+
+time_regex = re.compile(r"""(?:(?P<seconds>([0-9]+))\s*?(s|sec|secs|second|seconds))?
+                            (?:(?P<minutes>([0-9]+))\s*?(m|min|mins|minute|minutes))?
+                            (?:(?P<hours>([0-9]+))\s*?(h|hr|hrs|hour|hours))?
+                            (?:(?P<days>([0-9]+))\s*?(d|day|days))?
+                            (?:(?P<weeks>([0-9]+))\s*?(w|week|weeks))?
+                            (?:(?P<months>([0-9]+))\s*?(mo|month|months))?
+                            (?:(?P<years>([0-9]+))\s*?(y|yr|yrs|year|years))?""", re.I | re.X)
+
 
 def git_pull():
     return subprocess.run(["git", "pull"], capture_output=True)
+
+
+class TimeConverter(commands.Converter):
+    async def convert(self, ctx: utils.Context, arg: str):
+        conversions = {
+            "seconds": 0,
+            "minutes": 60,
+            "hours": 3600,
+            "days": 86400,
+            "weeks": 604800,
+            "years": 31557600
+        }
+
+        now = dt.utcnow()
+
+        if match := time_regex.finditer(arg):
+            times = {}
+            matches = [m.groupdict() for m in match if m.group(0)]
+            for match in matches:
+                for k, v in match.items():
+                    if v:
+                        times[k] = v
+
+            for k, v in times.items():
+                amount = conversions.get(k)
+                secs = v * amount
+                delta = datetime.timedelta(seconds=secs)
+                now += delta
+
+            return TimeReason("Test", now)
+
 
 class Owner(commands.Cog):
     """Commands for development."""
